21883de4b36ddaea7f3ea7557d1a959e14b4bc9d
Lee Kai Yang <kaiyang01007@gmail.com>
2023-12-27 23:24:33 +0800

    Update path for mackeral Fish

--- src/ocean/Fish.java
+++ src/ocean/Fish.java
@@ -9,16 +9,16 @@ import java.util.Map;
 public class Fish extends OceanCreature {
 
 	private final static Map<Color, List<String>> colorImagePathMap = Map.of(
-			Color.RED, List.of("images/crab-red.gif", "images/crab-red.gif"),
-			Color.ORANGE, List.of("images/crab-orange.gif", "images/crab-orange.gif"),
-			Color.YELLOW, List.of("images/crab-yellow.gif", "images/crab-yellow.gif"),
-			Color.GREEN, List.of("images/crab-green.gif", "images/crab-green.gif"),
-			Color.BLUE, List.of("images/crab-blue.gif", "images/crab-blue.gif"),
-			Color.PURPLE, List.of("images/crab-purple.gif", "images/crab-purple.gif")
+			Color.RED, List.of("images/mackeralL-red.gif", "images/mackeralR-red.gif"),
+			Color.ORANGE, List.of("images/mackeralL-orange.gif", "images/mackeralR-orange.gif"),
+			Color.GREEN, List.of("images/mackeralL-green.gif", "images/mackeralR-green.gif"),
+			Color.BLUE, List.of("images/mackeralL.gif", "images/mackeralR.gif"),
+			Color.PURPLE, List.of("images/mackeralL-purple.gif", "images/mackeralR-purple.gif")
 	);
 
 	public Fish() {
 		fishSize = 180;
+		type = OceanCreatureType.Fish;
 		leftimage = new Image("images/mackeralL.gif", fishSize, fishSize, true, true);
 		rightimage = new Image("images/mackeralR.gif", fishSize, fishSize, true, true);
 	}
